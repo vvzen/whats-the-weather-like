491c30d6e0751d094e2a2cf33c502881a0e6fc8b
Valerio Viperino <valerio.viperino@framestore.com>
2022-05-07 11:29:53 +0100

    Use FIFO pipe to avoid connection restart
    When a new serial connection is made, the Arduino UNO, by default, will
    be reset. So instead of making new connections every time, keep the
    connection alive and just read from a named pipe where the sender will
    talk to.

--- send_commands_to_arduino.py
+++ send_commands_to_arduino.py
@@ -1,23 +1,56 @@
 #!/Users/vvzen/miniconda3/envs/htwisj-env/bin/python
+import os
+import sys
 import time
 import argparse
 
 import serial
 
+ARDUINO_ADDRESS = "/dev/cu.usbmodem301"
+
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--value", type=str)
-    args = parser.parse_args()
-
-    value_bytes = args.value.encode("ascii")
-    value_bytes += b"\n"
-
-    device = serial.Serial("/dev/cu.usbmodem301", 9600)
+    parser = argparse.ArgumentParser(
+        description=("Reads from a FIFO pipe on disk and send the data"
+                     "to Arduino. The name of the pipe can be customized by "
+                     "using the HTWISJ_PIPE_NAME env variable."),
+    )
+    parser.parse_args()
 
     # Arduino resets on a new connection, wait a bit before writing
-    time.sleep(3)
-    device.write(value_bytes)
-    device.close()
+    # The alternative is to disable the reset:
+    # https://playground.arduino.cc/Main/DisablingAutoResetOnSerialConnection
+    device = serial.Serial(ARDUINO_ADDRESS, 9600)
+    time.sleep(2)
+
+    # We read from a pipe on disk (created via mkfifo)
+    pipe_name = os.getenv("HTWISJ_PIPE_NAME", "htwisj_pipe")
+    if not os.path.exists(pipe_name):
+        try:
+            print("Creating pipe on disk named '%s'" % pipe_name)
+            os.mkfifo(pipe_name)
+        except OSError as e:
+            print("Failed to create FIFO pipe named '%s'" % pipe_name)
+            sys.exit(1)
+
+    print("Opened pipe. Reading for messages..")
+    pipe = open(pipe_name, 'r')
+
+    try:
+        while True:
+            line = pipe.read()
+            if not line:
+                time.sleep(0.5)
+                continue
+
+            value_bytes = line.encode("ascii")
+            print("Sending %s" % value_bytes)
+            device.write(value_bytes)
+    except KeyboardInterrupt:
+        print("Received SIGINT. Shutting down..")
+    finally:
+        pipe.close()
+        device.close()
+
 
 if __name__ == "__main__":
     main()
